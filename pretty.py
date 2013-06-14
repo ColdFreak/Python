6d34a5c365e7febd4589f0d556ca23253c0891c2
Wang Zhijun <madfrogme@gmail.com>
2013-06-14 20:43:48 +0900

    keep changing parsing c code just forget about those fucking complicated stuff,do it my way

--- pretty.py
+++ pretty.py
@@ -4,24 +4,48 @@
 s = ""
 lastc = ''
 
+indent_keyword = ['{', ')', ';', ':', 'if', 'else', 'do' ]
 indent = 0
 
+for_flag = 0
 
-f = open("file.c")
+semicolon_count = 0
+
+f = open("boo.c")
 f2 = open("new.c", "w")
 
 
 lines = f.readlines()
 
 for line in lines:
-
-	if line.strip().startswith('#') :
+	
+	if line.strip().startswith('for'):
+		if '{' != line[len(line.strip())-1]:
+			indent += 1
+		for_flag = 1
+	
+	elif line.strip().startswith('/*') and line.strip().endswith('*/'):
+		s += '\n'
 		s += line
 		s += '\n'
+		s += '\t' * indent
+		continue
+	elif line.strip().startswith('#') :
+		s += '\n'
+		s += line
+		s += '\n'
+		s += '\t' * indent
 		continue	
-	if line in ['\n', '\r\n'] :
-	 	s += line
+	elif line in ['\n', '\r\n'] :
 	 	continue
+
+	if line.strip().endswith('*/') and not line.strip().startswith('/*'):
+		s += '\n'
+		s += '\t' * indent
+		s += line.strip()
+		s += '\n'
+		s += '\t' * indent
+		continue
 
 	li = line.split()
 
@@ -37,6 +61,13 @@ for line in lines:
 				s += '\t'* indent
 				
 			elif ch == ';':
+				if for_flag == 1 and semicolon_count < 2 :
+					semicolon_count += 1
+					s += ch
+					continue
+				else:
+					for_flag = 0
+					semicolon_count = 0
 				s += ch
 				s += '\n'
 				s += '\t'* indent
@@ -46,6 +77,8 @@ for line in lines:
 				s += '\n'
 				s += '\t' * indent
 				s += ch
+				s += '\n'
+				s += '\t' * indent
 				lastc = ch
 
 			elif lastc == '}':
@@ -68,8 +101,4 @@ for line in lines:
 
 
 f2.write(s)
-			
-		
-
-
 
